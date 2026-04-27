988e08e446da5a401e48c81daa6447e6421c4754
vrrroro <rohitsham50@gmail.com>
2026-04-28 00:58:06 +0530

    update base path to /mybaby/

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/game/',  // ← change 'game' to your GitHub repo name
+  base: '/mybaby/',  // ← change 'game' to your GitHub repo name
 })
